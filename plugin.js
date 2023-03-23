3994af5ba71915c347d25e879ecce0b571e29548
marcopiraccini <marco.piraccini@gmail.com>
2023-03-23 15:29:21 +0100

    test build args

--- plugin.js
+++ plugin.js
@@ -4,4 +4,9 @@
 /** @param {import('fastify').FastifyInstance} app */
 module.exports = async function (app) {
   console.log('@@@@@@@@@@new deploy - test logs @@@@@@@@@@')
+
+  app.get('/test-marco', async (request, reply) => {
+    app.log.info('test-marco')
+    return 'MARCO'
+  })
 }
